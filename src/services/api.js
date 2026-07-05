b7e3e7a6c3acb3776280d10ff15526d7ae42d524
Obada-Dalal <obadaeiddalal@gmail.com>
2026-07-06 00:01:42 +0300

    edit api

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,19 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://server-digital-menu.onrender.com/api",
-//   headers: {
-//     "Content-Type": "application/json"
-//   }
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
-  // بدون Content-Type - خلي axios يحدد تلقائياً
+  baseURL: "https://server-digital-menu.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api"
+//   // بدون Content-Type - خلي axios يحدد تلقائياً
+// });
+
+// export default api;
